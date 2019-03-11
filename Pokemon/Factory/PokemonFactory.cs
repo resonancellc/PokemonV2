7cb21c5ca73e6cc9b859ff55e01eb757150610ce
svCcare <maciej.pawlak94@gmail.com>
2019-03-11 23:07:27 +0100

    Moved functionality from PokemonGenerator to PokemonFactory

--- Pokemon/Factory/PokemonFactory.cs
+++ Pokemon/Factory/PokemonFactory.cs
@@ -15,35 +15,30 @@ namespace Pokemon.Factory
             return new Pokemon();
         }
 
-        public static IPokemon CreatePokemon(int level)
+        public static IPokemon CreatePokemon(int level, int id = 0)
         {
             IPokemon pokemon = new Pokemon();
             pokemon.Level = level;
 
-            // filtering the overall list of pokemons by level, we don't want lvl 5 charizard
-            var availablePokemons = PokemonList.Pokemons.Where(p => p.Value.MinimalLevel <= level);
-            // selecting random entry from filtred list
-            pokemon = availablePokemons
-                        .ElementAt(GenerateRandomNumber.GetRandomNumber(0, availablePokemons.Count()))
-                        .Value;
+            if (id > 0)
+            {
+                // obtaining the one specific pokemon
+                pokemon = PokemonList.Pokemons.Where(p => p.Key == id).FirstOrDefault().Value;
+            }
+            else
+            {
+                // filtering the overall list of pokemons by level, we don't want lvl 5 charizard
+                var availablePokemons = PokemonList.Pokemons.Where(p => p.Value.MinimalLevel <= level);
+                // selecting random entry from filtred list
+                pokemon = availablePokemons
+                            .ElementAt(GenerateRandomNumber.GetRandomNumber(0, availablePokemons.Count()))
+                            .Value;
+            }
 
             pokemon.Stats = PokemonStatsFactory.CreateStats(level, pokemon.Stats);
             pokemon.Attacks = PokemonAttacksFactory.GetAttacks(pokemon);
 
             return pokemon;
         } 
-
-        public static IPokemon CreatePokemon(int level, int id)
-        {
-            IPokemon pokemon = new Pokemon();
-            pokemon.Level = level;
-
-            pokemon = PokemonList.Pokemons.Where(p => p.Key == id).FirstOrDefault().Value;
-
-            pokemon.Stats = PokemonStatsFactory.CreateStats(level, pokemon.Stats);
-            pokemon.Attacks = PokemonAttacksFactory.GetAttacks(pokemon);
-
-            return pokemon;
-        }
     }
 }
