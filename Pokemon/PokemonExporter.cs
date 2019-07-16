9b07083f96b0c30c49f84fe9fd4f2efb9787eaa8
svCcare <maciej.pawlak94@gmail.com>
2019-07-16 20:29:10 +0200

    Exporting to file

--- Pokemon/PokemonExporter.cs
+++ Pokemon/PokemonExporter.cs
@@ -13,16 +13,34 @@ namespace Pokemon
     public class PokemonExporter
     {
         IEnumerable<IPokemon> _pokemonsToExport;
+        string _filePath;
 
-        public PokemonExporter(IEnumerable<IPokemon> pokemonsToExport)
+        public PokemonExporter(IEnumerable<IPokemon> pokemonsToExport, string filePath)
         {
             _pokemonsToExport = pokemonsToExport;
+            _filePath = filePath;
         }
 
         public bool Export()
         {
-            var json = Newtonsoft.Json.JsonConvert.SerializeObject(_pokemonsToExport);
-            return true;
+            try
+            {
+                var json = Newtonsoft.Json.JsonConvert.SerializeObject(_pokemonsToExport);
+                string path = $@"{_filePath}";
+
+                // This text is added only once to the file.
+                if (!File.Exists(path))
+                {
+                    // Create a file to write to.
+                    File.WriteAllText(path, json);
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
