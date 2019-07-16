82b22367809fa503e901b6e4c2f25bb5c2c5cd9b
svCcare <maciej.pawlak94@gmail.com>
2019-07-16 20:43:26 +0200

    Renamed PokemonExporter class, added PokemonImport with logic

---
+++ Pokemon/PokemonImport.cs
@@ -0,0 +1,34 @@
+﻿using Pokemon.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon
+{
+    public class PokemonImport
+    {
+        private readonly string _filePath;
+
+        public PokemonImport(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public IEnumerable<IPokemon> Import()
+        {
+            try
+            {
+                string importJson = File.ReadAllText(_filePath);
+                return Newtonsoft.Json.JsonConvert.DeserializeObject<IEnumerable<IPokemon>>(importJson);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
