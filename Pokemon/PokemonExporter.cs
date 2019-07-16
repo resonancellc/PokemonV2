a3c112947210340346352ed0be6886ba07fdc497
svCcare <maciej.pawlak94@gmail.com>
2019-07-16 19:08:50 +0200

    Added Exporter with logic, awaits further implementation - to external file

---
+++ Pokemon/PokemonExporter.cs
@@ -0,0 +1,29 @@
+﻿using Pokemon.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace Pokemon
+{
+    public class PokemonExporter
+    {
+        IEnumerable<IPokemon> _pokemonsToExport;
+
+        public PokemonExporter(IEnumerable<IPokemon> pokemonsToExport)
+        {
+            _pokemonsToExport = pokemonsToExport;
+        }
+
+        public bool Export()
+        {
+            //var json = new JavaScriptSerializer().Serialize(_pokemonsToExport);
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(_pokemonsToExport);
+            return true;
+        }
+    }
+}
