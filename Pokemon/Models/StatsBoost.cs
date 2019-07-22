cada72afcb070218ffebfb60b814e8dcbc043581
svCcare <maciej.pawlak94@gmail.com>
2019-07-22 21:58:26 +0200

    Added StatsBoost model, added mapper for StatsBoost

---
+++ Pokemon/Models/StatsBoost.cs
@@ -0,0 +1,11 @@
+﻿namespace Pokemon.Models
+{
+    public class StatsBoost
+    {
+        public Target Target { get; set; }
+
+        public StatType StatType { get; set; }
+
+        public int Value { get; set; }
+    }
+}
---
+++ Pokemon/ObjectMappers/StatsBoostMapper.cs
@@ -0,0 +1,20 @@
+﻿using Pokemon.Models;
+using System;
+
+namespace Pokemon.ObjectMappers
+{
+    public static class StatsBoostMapper
+    {
+        public static StatsBoost ToDomainObject(string boostString)
+        {
+            string[] splittedBoosts = boostString.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            StatsBoost boost = new StatsBoost()
+            {
+                Target = splittedBoosts[0] == "self" ? Target.Self : Target.Enemy,
+                StatType = (StatType)Int32.Parse(splittedBoosts[1]),
+                Value = Int32.Parse(splittedBoosts[2])
+            };
+            return boost;
+        }
+    }
+}
