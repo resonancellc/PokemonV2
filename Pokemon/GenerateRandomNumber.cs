aaf5e2142d708649a7071ebfb16f2a1857dbfb19
svCcare <maciej.pawlak94@gmail.com>
2019-07-28 21:12:32 +0200

    Updated code of RandomNumberGenerator to reflect current standards

--- Pokemon/GenerateRandomNumber.cs
+++ Pokemon/GenerateRandomNumber.cs
@@ -1,21 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Pokemon
 {
     public static class GenerateRandomNumber
     {
-        private static readonly Random random = new Random();
-        private static readonly object syncLock = new object();
-        //CalculatorHelper.RandomNumber()
+        private static readonly Random _random = new Random();
+        private static readonly object _syncLock = new object();
+
         public static int GetRandomNumber(int min, int max)
         {
-            lock (syncLock)
+            lock (_syncLock)
             { // synchronize
-                return random.Next(min, max);
+                return _random.Next(min, max);
             }
         }
     }
