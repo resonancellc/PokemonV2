58c0b16899173d64aceebb39ca2d43b3ce5edba7
svCcare <maciej.pawlak94@gmail.com>
2019-08-01 22:16:49 +0200

    Removed ChangeStatus method since we can just set property Condition of Pokemon

--- Pokemon/AdditionalEffects/StatusChanger.cs
+++ Pokemon/AdditionalEffects/StatusChanger.cs
@@ -1,5 +1,4 @@
 ﻿using Pokemon.Models;
-using Pokemon.Calculators;
 
 namespace Pokemon.AdditionalEffects
 {
@@ -16,71 +15,5 @@ namespace Pokemon.AdditionalEffects
         public int? SecondaryParameter { get; set; }
 
         public bool IsOnSelf { get; set; }
-
-        public void ChangeStatus(IPokemon pokemon)
-        {
-            if (Name.Contains("Poison"))
-            {
-                if (ChanceCalculator.CalculateChance((int)PrimaryParameter, 100))
-                {
-                    BattleLog.AppendText($"{pokemon.Name} is now poisoned");
-                    pokemon.Condition = Condition.PSN;
-                }
-            }
-            else if (Name.Contains("Burn"))
-            {
-                if (ChanceCalculator.CalculateChance((int)PrimaryParameter, 100))
-                {
-                    BattleLog.AppendText($"{pokemon.Name} is now burning");
-                    pokemon.Condition = Condition.BRN;
-                }
-            }
-            else if(Name.Contains("Paralysis"))
-            {
-                if(pokemon.Condition != 0)
-                {
-                    BattleLog.AppendText($"{pokemon.Name} is unaffected");
-                    return;
-                }
-                if (ChanceCalculator.CalculateChance((int)PrimaryParameter, 100))
-                {
-                    BattleLog.AppendText($"{pokemon.Name} is now paralysed");
-                    pokemon.Condition = Condition.PAR;
-                }
-            }
-            else if (Name.Contains("Sleep"))
-            {
-                if (pokemon.Condition != 0)
-                {
-                    BattleLog.AppendText($"{pokemon.Name} is unaffected");
-                    return;
-                }
-                if (ChanceCalculator.CalculateChance((int)PrimaryParameter, 100))
-                {
-                    BattleLog.AppendText($"{pokemon.Name} is now asleep");
-                    pokemon.Condition = Condition.SLP;
-                }
-            }
-            else if(Name.Contains("Confusion"))
-            {
-                if (pokemon.Condition == Condition.SLP || pokemon.IsConfused)
-                {
-                    BattleLog.AppendText($"{pokemon.Name} is unaffected");
-                    return;
-                }
-                if (ChanceCalculator.CalculateChance((int)PrimaryParameter, 100))
-                {
-                    BattleLog.AppendText($"{pokemon.Name} is now confused");
-                    pokemon.IsConfused = true;
-                }
-            }
-            else if(Name.Contains("Flinch"))
-            {
-                if (ChanceCalculator.CalculateChance((int)PrimaryParameter, 100))
-                {
-                    pokemon.IsFlinched = true;
-                }
-            }
-        }
     }
 }
