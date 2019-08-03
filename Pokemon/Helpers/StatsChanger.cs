810da6c2c1b719ef4cf7795445196fa9d2e3a3c7
svCcare <maciej.pawlak94@gmail.com>
2019-08-03 11:09:12 +0200

    Changed the return type of ChangeTempStats to string so we don't need to inject battlelog

--- Pokemon/Helpers/StatsChanger.cs
+++ Pokemon/Helpers/StatsChanger.cs
@@ -5,9 +5,11 @@ namespace Pokemon.Helpers
 {
     public static class StatsChanger
     {
-        public static bool ChangeTempStats(IAttack attack, IPokemon attackingPokemon, IPokemon targetPokemon)
+        public static string ChangeTempStats(IAttack attack, IPokemon attackingPokemon, IPokemon targetPokemon)
         {
             string[] boosts = attack.BoostStats.SplitBoosts();
+            string output = string.Empty;
+
             foreach (string boostString in boosts)
             {
                 StatsBoost statsBoost = StatsBoostMapper.ToDomainObject(boostString);
@@ -19,14 +21,15 @@ namespace Pokemon.Helpers
                 if (statsChangeValidator.StatChangePossible())
                 {
                     ChangeTempPokemonStats(affectedPokemon, statsBoost);
-                    return true;
+                    string changeType = statsBoost.Value > 0 ? "increased" : "decreased";
+                    output += $"{affectedPokemon.Name} {statsBoost.StatType.ToString()} {changeType}\n"; 
                 }
                 else
                 {
-                    return false;
+                    return $"{affectedPokemon.Name} {statsBoost.StatType.ToString()} cannot go any higher";
                 }
             }
-            return true;
+            return output;
         }
 
         public static void ChangeTempPokemonStats(IPokemon affectedPokemon, StatsBoost statsBoost)
